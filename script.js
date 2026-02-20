ad6aacbd5a17fc6c9e5e858e3738a13ae5b2755e
T1SPIDEY <workdevanshaga@gmail.com>
2026-02-20 10:32:11 +0530

    addeD

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+ // console.log("hello world");
+// document.write("hello world");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num = "devansh";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+const syml= Symbol(4);
+console.log(syml);
+const sym2 =Symbol(4);
+console.log(sym2);
+if(syml===sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+var a=100; var b=15; var c=10; var linebreak="<br>";
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
